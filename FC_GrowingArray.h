c45804321d8877f1de8c0e1a7a17cd4a1c1cca49
JanWielgus <jan.wielgus12@gmail.com>
2020-04-08 16:53:16 +0200

    FC_GrowingArray template class added

---
+++ FC_GrowingArray.h
@@ -0,0 +1,119 @@
+// FC_GrowingArray.h
+/*
+    Created:	08/04/2020
+    Author:     Jan Wielgus
+*/
+
+
+#ifndef _FC_GROWINGARRAY_h
+#define _FC_GROWINGARRAY_h
+
+#include "arduino.h"
+
+
+template <class T>
+class FC_GrowingArrayClass
+{
+private:
+    T* array;
+    uint16_t size;
+    T nullItem; // returns that item for example if index is out of bounds
+
+public:
+    FC_GrowingArrayClass()
+    {
+        size = 0;
+    }
+
+
+    ~FC_GrowingArrayClass()
+    {
+        if (size > 0)
+            delete[] array;
+    }
+
+
+    // copy constructor
+    FC_GrowingArrayClass(const FC_GrowingArrayClass& other)
+    {
+        size = other.size;
+
+        // copy data
+        if (size > 0)
+        {
+            array = new T[size];
+
+            for (int i = 0; i < size; i++)
+                array[i] = other.array[i];
+        }
+    }
+
+
+    // overloaded assignment operator
+    FC_GrowingArrayClass& operator=(const FC_GrowingArrayClass& other)
+    {
+        if (this != &other)// self-assignment check expected
+        {
+            if (other.size != size)           // storage cannot be reused
+            {
+                delete[] array;               // destroy storage in this
+                array = new T[other.size];    // create storage in this
+                size = other.size;
+            }
+
+            for (int i = 0; i < size; i++)
+                array[i] = other.array[i];
+        }
+
+        return *this;
+    }
+
+
+    void add(T item)
+    {
+        // create bigger array
+        T* newArray = new T[size + 1];
+
+        // copy data
+        for (int i = 0; i < size; i++)
+            newArray[i] = array[i];
+
+        // put new item on the end of new array
+        newArray[size] = item;
+        size++;
+
+        // delete old array and change to the new pointer
+        delete[] array;
+        array = newArray;
+    }
+
+
+    T& get(uint16_t index)
+    {
+        if (index < size)
+            return array[index];
+        return nullItem;
+    }
+
+
+    T& operator[](uint16_t index)
+    {
+        return get(index);
+    }
+
+
+    T* getArray()
+    {
+        return array;
+    }
+
+
+    uint16_t getSize()
+    {
+        return size;
+    }
+};
+
+
+#endif
+
---
+++ FC_GrowingArray_example/FC_GrowingArray_example.ino
@@ -0,0 +1,55 @@
+/*
+    Name:       FC_GrowingArray_example.ino
+    Created:	08/04/2020
+    Author:     Jan Wielgus
+*/
+
+#include <FC_GrowingArray.h>
+
+// something that pretend unit tests
+bool test1();
+
+
+void setup()
+{
+
+
+}
+
+void loop()
+{
+
+}
+
+
+bool test1()
+{
+    FC_GrowingArrayClass<uint16_t>* testArray = new FC_GrowingArrayClass<uint16_t>;
+
+    testArray->add(1);
+    testArray->add(2);
+    testArray->add(11);
+    testArray->add(22);
+    testArray->add(122);
+    testArray->add(215);
+    testArray->add(1543);
+    testArray->add(532);
+
+    if (testArray->getSize() != 8)
+        return false;
+    if (testArray->get(0) != 1)
+        return false;
+    if (testArray->get(6) != 1543)
+        return false;
+    if ((*testArray)[7] != 532)
+        return false;
+    if (testArray->getArray()[3] != 22)
+        return false;
+
+
+    FC_GrowingArrayClass<uint16_t> deepCopy = *testArray;
+    delete testArray;
+    if (deepCopy[1] != 2)
+        return false;
+    
+}
