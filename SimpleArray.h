5df92c3dd1e0f42233468c0f7383d6c65a93133c
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-03 17:43:11 +0200

    Created SimpleArray, concrete class of IArray interface

---
+++ SimpleArray.h
@@ -0,0 +1,127 @@
+/**
+ * @file SimpleArray.h
+ * @author Jan Wielgus
+ * @brief Implementation of simple C++ library for IArray interface.
+ * @date 2020-08-03
+ * 
+ */
+
+#ifndef SIMPLEARRAY_H
+#define SIMPLEARRAY_H
+
+#include <IArray.h>
+
+
+template <class T>
+class SimpleArray : IArray<T>
+{
+private:
+    T* array = nullptr;
+    const size_t MaxSize;
+    size_t arraySize = 0; // amt of elements in the array
+    T null_item; // returned if provided index is out of bounds
+
+
+public:
+    SimpleArray(size_t arrayMaxSize)
+        : MaxSize(arrayMaxSize)
+    {
+        if (MaxSize > 0)
+            array = new T[MaxSize];
+        arraySize = 0;
+    }
+
+
+    ~SimpleArray()
+    {
+        if (MaxSize > 0)
+            delete [] array;
+    }
+
+
+    bool add(const T& item) override
+    {
+        if (isFull())
+            return false;
+        
+        array[arraySize] = item;
+        return true;
+    }
+
+
+    bool add(const T& item, size_t index) override
+    {
+        if (isFull())
+            return false;
+        
+        // Make place for a new item
+        for (size_t i = arraySize; i > index; i--)
+            array[i] = array[i-1];
+        
+        array[index] = item;
+        return true;
+    }
+
+
+    T& get(size_t index) override
+    {
+        if (index < arraySize)
+            return array[index];
+        return null_item;
+    }
+
+
+    const T& get(size_t index) const override
+    {
+        if (index < arraySize)
+            return array[index];
+        return null_item;
+    }
+
+
+    T& operator[](size_t index) override
+    {
+        return get(index);
+    }
+
+
+    const T& operator[](size_t index) const override
+    {
+        return get(index);
+    }
+
+
+    bool replace(size_t index, const T& newItem) override
+    {
+        if (index >= arraySize)
+            return false;
+        
+        array[index] = newItem;
+        return true;
+    }
+
+
+    size_t getSize() const override
+    {
+        return arraySize;
+    }
+
+
+    bool isFull() override
+    {
+        if (arraySize >= MaxSize)
+            return true;
+        return false;
+    }
+
+
+    bool isEmpty() override
+    {
+        if (arraySize == 0)
+            return true;
+        return false;
+    }
+};
+
+
+#endif
